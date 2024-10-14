51f892b97aaea5c4cbddcdc6dea8756e1bc97bfb
Gabi Moldovan <moldovangabi91@yahoo.com>
2024-10-14 21:20:44 +0300

    Ex2 without main

--- Assig1/src/Main.java
+++ Assig1/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Ex1
         Ex1 uni = new Ex1();
         int[] grades = {48, 23, 38, 60, 88, 99, 97, 66};
 
@@ -18,5 +19,14 @@ public class Main {
         }
 
         System.out.println("Vierte Methode: " + uni.maxGrade(grades));
+
+        //Ex2
+        Ex2 arr = new Ex2();
+        System.out.println("Max: " + arr.max(grades));
+        System.out.println("Min: " + arr.min(grades));
+        System.out.println("Max Summe: " + arr.maxSum(grades));
+        System.out.println("Min Summe: " + arr.minSum(grades));
+
+        //Ex3
     }
 }
